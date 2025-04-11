a6909c777cb693cb08f47009c38d094e84e939a4
rtorokk <rachy.torok@gmail.com>
2025-04-10 20:28:53 -0400

    Setup root component with state and props structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import React, { useState } from 'react';
+import Gallery from './components/Gallery';
+
+// Root component of the app
+function App() {
+  // Global state to hold the list of tours
+  const [tours, setTours] = useState([]);
+
+  // Function to remove a tour by its id
+  const removeTour = (id) => {
+    setTours((prevTours) => prevTours.filter((tour) => tour.id !== id));
+  };
+
+  return (
+    <main>
+      <h1>Tour Explorer</h1>
+      {/* Pass state and handler to Gallery component */}
+      <Gallery tours={tours} setTours={setTours} onRemove={removeTour} />
+    </main>
+  );
+}
+
+export default App;
 
 
 
